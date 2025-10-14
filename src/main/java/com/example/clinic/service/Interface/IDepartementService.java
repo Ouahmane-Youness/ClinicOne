89af409f022684af4c858e8e5f67493a20db4f98
youness <youness.ouahmane4@gmail.com>
2025-10-14 11:14:07 +0100

    add departement interface service

--- src/main/java/com/example/clinic/service/Interface/IDepartementService.java
+++ src/main/java/com/example/clinic/service/Interface/IDepartementService.java
@@ -1,4 +1,31 @@
 package com.example.clinic.service.Interface;
 
-public class IDepartementService {
+import com.example.clinic.entities.Departement;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IDepartementService {
+
+    Departement createDepartement(String nom);
+
+    Optional<Departement> getDepartementById(Long id);
+
+    Optional<Departement> getDepartementByNom(String nom);
+
+    List<Departement> getAllDepartements();
+
+    List<Departement> searchDepartementsByNom(String nom);
+
+    Departement getDepartementWithDocteurs(Long id);
+
+    Departement updateDepartement(Long id, String nom);
+
+    void deleteDepartement(Long id);
+
+    long getTotalDepartements();
+
+    boolean departementExists(String nom);
+
+    int getDocteurCount(Long departementId);
 }
